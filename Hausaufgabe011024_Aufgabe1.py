79271c4b3cf77ecfcbb66abab022e14cbef9194f
affan14 <affan.ahmad@yahoo.de>
2024-10-01 18:57:26 +0200

    first commit

---
+++ Hausaufgabe011024_Aufgabe1.py
@@ -0,0 +1,13 @@
+# Ein Programm zur Eingabe der persönlichen Daten
+
+# Definieren der Variablen
+first_name = "Affan"  # String
+last_name = "Ahmad"   # String
+birth_year = 1975     # Ganzzahl
+has_licence = True    # Boolean (True oder False)
+
+# Ausgabe der Variablen
+print("Vorname:", first_name)
+print("Nachname:", last_name)
+print("Geburtsjahr:", birth_year)
+print("Führerschein gültig?:", has_licence)
